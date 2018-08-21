3507d4915f4e71b3a05b948a41cb30a99e5b98a7
Bishram <bishram.munda78>
2018-08-21 21:45:50 +0530

    Commit #2: Splash Screen, Launcher icon added. Splash screen designed and implemented in the app. Splash screen will remain for 3 seconds and finishes by taking the focus to the MainActivity.

--- app/src/main/java/com/bishram/tour/guide/SplashActivity.java
+++ app/src/main/java/com/bishram/tour/guide/SplashActivity.java
@@ -1,13 +1,30 @@
 package com.bishram.tour.guide;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.bishram.tour.guide.activities.MainActivity;
+
 public class SplashActivity extends AppCompatActivity {
 
+    private static final int MILLIS_DELAYED = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        loadMainActivity();
+    }
+
+    private void loadMainActivity() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, MILLIS_DELAYED);
     }
 }
